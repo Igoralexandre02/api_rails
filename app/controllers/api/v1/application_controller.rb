fd3d4c0b09e265fd58544cfaf0d5e3ffd9105bda
IgorAlexandre <igorvieira@seduc.ro.gov.br>
2025-09-19 13:07:36 -0400

    alteracoes  temporarias

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -7,10 +7,10 @@ module Api
 
       def authenticate_with_api_key
         # 1. Tenta pegar a chave do cabeçalho da requisição (ex: 'X-API-KEY')
-        api_key_from_header = request.headers["X-Api-Key"].strip if request.headers["X-Api-Key"]
+        api_key_from_header = request.headers["X-Api-Key"]&.strip if request.headers["X-Api-Key"]
 
         # 2. Compara a chave do cabeçalho com a chave armazenada em .env
-        unless api_key_from_header == ENV["API_KEY"].strip
+        unless api_key_from_header == ENV["API_KEY"]&.strip
           # 3. Se a chave for inválida, retorna um erro 401 Unauthorized
           render json: { error: "Unauthorized: Invalid API key" }, status: :unauthorized
         end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_17_124906) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "enderecos", force: :cascade do |t|
     t.integer "cep"
     t.string "logradouro"
