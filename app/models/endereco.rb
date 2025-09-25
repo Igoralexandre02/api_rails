8b827172c01ca1b26160fabf330e93c2f46de8d7
IgorAlexandre <igorvieira@seduc.ro.gov.br>
2025-09-25 08:28:40 -0400

    validacoes adicionadas as models endereco e pessoa

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,2 +1,6 @@
 class Endereco < ApplicationRecord
+  validates :cep, :logradouro, :numero, :cidade, presence: true
+  validates :cep, :numero, uniqueness: true
+  validates :cep, format: { with: /\A\d+\z/ }
+  validates :numero, format: { with: /\A\d+\z/ }
 end
--- app/models/pessoa.rb
+++ app/models/pessoa.rb
@@ -1,4 +1,11 @@
 class Pessoa < ApplicationRecord
   belongs_to :endereco, optional: true
+  validates_associated :endereco
   accepts_nested_attributes_for :endereco
+
+  validates :nome, :cpf, :email, :telefone, presence: true
+  validates :cpf, uniqueness: true
+  validates :cpf, length: { is: 11 }
+  validates :cpf, format: { with: /\A\d+\z/ }
+  validates :telefone, format: { with: /\A\d+\z/ }
 end
